f6a460197f2a2a7ab7c766837f52bd5bfb5a4025
Kim Nahyeon <nhyeonii@naver.com>
2025-10-16 21:37:53 +0900

    [FEAT] #2 MainScreen 구현

--- app/src/main/java/com/sopt/dive/MainActivity.kt
+++ app/src/main/java/com/sopt/dive/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.dive.core.component.InfoItem
 import com.sopt.dive.ui.theme.DiveTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +29,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiveTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    /*Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    )*/
                 }
             }
         }
@@ -31,17 +40,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MainScreen(
+    id: String,
+    password: String,
+    nickname: String,
+    mbti: String,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.fillMaxSize()
+            .background(color = Color(0xFFFAFAFA))
+            .padding(16.dp)
+            .padding(top = 34.dp)
+            .navigationBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = null
+        )
+
+        Text("김나현")
+
+        InfoItem(
+            label = "ID",
+            value = id
+        )
+        InfoItem(
+            label = "Password",
+            value = password
+        )
+        InfoItem(
+            label = "Nickname",
+            value = nickname
+        )
+        InfoItem(
+            label = "MBTI",
+            value = mbti
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun MainScreenPreview() {
     DiveTheme {
-        Greeting("Android")
+        MainScreen(
+            id = "testUser",
+            password = "1234",
+            nickname = "테스트",
+            mbti = "ISTJ"
+        )
     }
 }
