b9dd8794faab23b7713e16ac35d17bbafd6d906c
Kim Nahyeon <nhyeonii@naver.com>
2025-10-16 20:45:50 +0900

    [FEAT] #2 SoptBasicButton 생성

---
+++ app/src/main/java/com/sopt/dive/core/component/SoptBasicButton.kt
@@ -0,0 +1,46 @@
+package com.sopt.dive.core.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.dive.core.extension.noRippleClickable
+import com.sopt.dive.ui.theme.DiveTheme
+
+@Composable
+fun SoptBasicButton(
+    title: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = Color.Black)
+            .noRippleClickable(onClick = onClick)
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = title, color = Color.White)
+    }
+}
+
+@Preview
+@Composable
+private fun SoptBasicButtonPreview() {
+    DiveTheme {
+        SoptBasicButton(
+            title = "로그인하기",
+            onClick = {}
+        )
+    }
+}
