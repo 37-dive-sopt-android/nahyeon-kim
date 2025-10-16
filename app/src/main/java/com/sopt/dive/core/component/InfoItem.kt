fcf5246b5c6463b5e7021598c7e547a742d970d5
Kim Nahyeon <nhyeonii@naver.com>
2025-10-16 20:46:26 +0900

    [FEAT] #2 InfoItem 생성

---
+++ app/src/main/java/com/sopt/dive/core/component/InfoItem.kt
@@ -0,0 +1,35 @@
+package com.sopt.dive.core.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InfoItem(
+    label: String,
+    value: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 12.dp)
+    ) {
+        Text(
+            text = label,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = value,
+            fontSize = 16.sp,
+            color = Color.Gray
+        )
+    }
+}
