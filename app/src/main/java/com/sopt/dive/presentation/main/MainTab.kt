58f5a31a8ae3c443e7748eba3036d663a111ba4d
Kim Nahyeon <nhyeonii@naver.com>
2025-10-30 16:44:48 +0900

    [FEAT] #4 MainBottomBar 구현

---
+++ app/src/main/java/com/sopt/dive/presentation/main/MainTab.kt
@@ -0,0 +1,30 @@
+package com.sopt.dive.presentation.main
+
+import androidx.annotation.DrawableRes
+import com.sopt.dive.R
+import com.sopt.dive.core.navigation.MainTabRoute
+import com.sopt.dive.presentation.home.navigation.Home
+import com.sopt.dive.presentation.mypage.navigation.MyPage
+import com.sopt.dive.presentation.search.navigation.Search
+
+enum class MainTab(
+    @DrawableRes val iconRes: Int,
+    val route: MainTabRoute,
+    val label: String
+) {
+    HOME(
+        iconRes = R.drawable.ic_home,
+        route = Home,
+        label = "홈"
+    ),
+    SEARCH(
+        iconRes = R.drawable.ic_search,
+        route = Search,
+        label = "검색"
+    ),
+    MYPAGE(
+        iconRes = R.drawable.ic_my,
+        route = MyPage,
+        label = "마이페이지"
+    )
+}
