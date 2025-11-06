af89b63424ed2bc0f4c53bcdc2078bd4aaa84e25
Kim Nahyeon <nhyeonii@naver.com>
2025-11-06 20:48:28 +0900

    [FEAT] #6 FlipCard 구현

--- app/src/main/java/com/sopt/dive/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/sopt/dive/presentation/search/SearchScreen.kt
@@ -1,11 +1,16 @@
 package com.sopt.dive.presentation.search
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.dive.core.designsystem.theme.DiveTheme
+import com.sopt.dive.presentation.search.component.FlipCard
 
 @Composable
 fun SearchRoute(
@@ -20,9 +25,18 @@ fun SearchRoute(
 private fun SearchScreen(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(
-            text = "Search"
-        )
+    Column(
+        modifier = modifier.background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FlipCard()
+    }
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+    DiveTheme {
+        SearchScreen()
     }
 }
