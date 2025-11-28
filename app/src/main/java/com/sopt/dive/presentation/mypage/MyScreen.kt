3518097436a16051dfcf22ba7fc395f1239e83f6
Kim Nahyeon <nhyeonii@naver.com>
2025-11-28 22:22:46 +0900

    [DEL] #10 안쓰는 코드 삭제

--- app/src/main/java/com/sopt/dive/presentation/mypage/MyScreen.kt
+++ app/src/main/java/com/sopt/dive/presentation/mypage/MyScreen.kt
@@ -67,11 +67,7 @@ fun MyPageRoute(
             )
         }
 
-        is UiState.Failure -> {
-            LaunchedEffect(Unit) {
-                Toast.makeText(context, "사용자 정보를 불러오는데 실패했습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
+        is UiState.Failure -> {}
 
         else -> {}
     }
