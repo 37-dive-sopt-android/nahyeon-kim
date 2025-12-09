10d3f8d1a6cfcfc1db045153a60b4d8d1b775f51
Kim Nahyeon <nhyeonii@naver.com>
2025-12-09 16:02:56 +0900

    [REF] #10 FakeDataSource 구현

--- app/src/main/java/com/sopt/dive/core/data/di/DataSourceModule.kt
+++ app/src/main/java/com/sopt/dive/core/data/di/DataSourceModule.kt
@@ -3,9 +3,9 @@ package com.sopt.dive.core.data.di
 import com.sopt.dive.core.data.datasource.AuthDataSource
 import com.sopt.dive.core.data.datasource.OpenDataDataSource
 import com.sopt.dive.core.data.datasource.UserDataSource
-import com.sopt.dive.core.data.datasourceimpl.AuthDataSourceImpl
+import com.sopt.dive.core.data.datasourceimpl.FakeAuthDataSourceImpl
+import com.sopt.dive.core.data.datasourceimpl.FakeUserDataSourceImpl
 import com.sopt.dive.core.data.datasourceimpl.OpenDataDataSourceImpl
-import com.sopt.dive.core.data.datasourceimpl.UserDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -18,11 +18,11 @@ abstract class DataSourceModule {
 
     @Binds
     @Singleton
-    abstract fun bindUserDataSource(userDataSourceImpl: UserDataSourceImpl): UserDataSource
+    abstract fun bindUserDataSource(fakeUserDataSourceImpl: FakeUserDataSourceImpl): UserDataSource
 
     @Binds
     @Singleton
-    abstract fun bindAuthDataSource(authDataSourceImpl: AuthDataSourceImpl): AuthDataSource
+    abstract fun bindAuthDataSource(fakeAuthDataSourceImpl: FakeAuthDataSourceImpl): AuthDataSource
 
     @Binds
     @Singleton
